d911783e3e28853ca18ed7aff00f9e25ccac35ad
Jaison Titus <jaison@hasura.io>
2017-09-07 21:53:39 +0530

    Added new Auth Providers

---
+++ Source/MobileAuthProvider.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Hasura
+//
+//  Created by Jaison on 07/09/17.
+//  Copyright © 2017 Hasura. All rights reserved.
+//
+
+import Foundation
---
+++ Source/UsernameAuthProvider.swift
@@ -0,0 +1,9 @@
+//
+//  UsernameAuthProvider.swift
+//  Hasura
+//
+//  Created by Jaison on 07/09/17.
+//  Copyright © 2017 Hasura. All rights reserved.
+//
+
+import Foundation
